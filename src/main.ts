b9ad1d48bce1836ca391bad9ac384139f2b8b1fa
SR Tamim <saifur.rahman.tamim1@gmail.com>
2025-04-26 00:22:44 +0600

    serverless aws lambda setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+type EventPayload = {
+  [key: string]: any;
+};
+
+export const handler: Handler = async (
+  event: EventPayload,
+  context: Context,
+  callback: Callback,
+) => {
+  if (event.path === '' || event.path === undefined) event.path = '/';
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
